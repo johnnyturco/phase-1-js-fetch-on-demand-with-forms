69066a93502bccc7c2344ff85e9510be4fd0e98c
Johnny <johnnyturco@me.com>
2022-11-04 15:13:06 -0600

    commit tree 52e202f5c4194862d71a5e58fbe83e548c6188b6

--- src/index.js
+++ src/index.js
@@ -1,5 +1,18 @@
 const init = () => {
-  
+  const inputForm = document.querySelector('form');
+  inputForm.addEventListener('submit', event => {
+    event.preventDefault();
+    const formInput = event.target.children[1].value;
+    // console.log(formInput);
+
+    fetch(`http://localhost:3000/movies/${formInput}`)
+      .then(response => response.json())
+      .then(movies => {
+        const title = document.querySelector("section#movieDetails h4").innerText = movies.title
+        const summary = document.querySelector("section#movieDetails p").innerText = movies.summary
+      });
+
+  });
 }
 
 document.addEventListener('DOMContentLoaded', init);
